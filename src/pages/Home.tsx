ebec88639196b6ccce4a86fc755544a363aca18a
Isaac Abakah <ikaydreams108@gmail.com>
2025-09-25 14:59:07 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -359,7 +359,7 @@ const Home: FC = () => {
                 transition={{ duration: 0.8, delay: 0.7, ease: [0.25, 0.46, 0.45, 0.94] }}
               >
                 Reduce file sizes instantly without compromising quality.
-                <br/>Fast, secure, and completely free compression for images, PDFs, and PPTX.
+                <br/>Fast and secure compression for images, PDFs, and PPTX.
               </motion.p>
             </motion.div>
             <motion.div
