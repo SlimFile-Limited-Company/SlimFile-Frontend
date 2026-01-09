3ceae3c96c16007747e87fe27634637fcff34719
ikaydreams-dev <ikaydreams108@gmail.com>
2026-01-09 12:05:30 +0000

    Fixed issue in main

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import App from './App';
 import Login from './pages/Login';
