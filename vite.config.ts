ab6311055d3f92e3182906498afefdca49b4d2e5
Isaac Abakah <ikaydreams108@gmail.com>
2026-02-10 02:42:58 +0000

    fixed workbox issue

--- vite.config.ts
+++ vite.config.ts
@@ -60,7 +60,8 @@ export default defineConfig(({ mode }) => ({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024 // 10MB (future-proof)
+, // 3 MB
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
