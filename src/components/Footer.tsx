80cf6d1aa4f05c6f2e689446f29f4e298afe5014
selorm <selormsem48@gmail.com>
2026-09-17 17:41:11 +0000

    added company website link in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,6 +39,7 @@ export const Footer = () => {
       links: [
         { label: "About", path: "/about" },
         // { label: "Partnerships", path: "/partnerships" },
+        { name: "Website", href: "https://company.slim-file.com", external: true },
         { label: "SDGs", path: "/sdgs" },
         { label: "News", path: "/news" },
         { label: "Why Compression?", path: "/file-compression-education" },
